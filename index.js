07ca5e92988fb193e4430cd6234f092369db30c6
alchemist1111 <emoyoeugine@gmail.com>
2024-04-02 21:01:36 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+// Wait for the DOM content to be loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Change the text content of the paragraph element
+    const paragraph = document.querySelector('p');
+    paragraph.textContent = "This is really cool!";
+});
